7465ff740b2ec3c58dddb1e9175f3ca0ea677463
jgsg3009 <jgsg3009@innorules.com>
2020-06-23 10:06:44 +0900

    불필요한 코드 삭제

--- CaptchaProcessing.py
+++ CaptchaProcessing.py
@@ -7,39 +7,47 @@ import base64
 import cv2
 import sys
 
-# 사이트 늘어날 때 마다 추가
-sys.path.insert(0, './법원')
-sys.path.insert(0, './민원24')
-from 법원.predict import PredictCaptcha
-from predictCaptcha
+import tensorflow as tf
+
+from BeobWon.predictCaptcha import PredictCaptcha as BeobWonPredictCaptcha
+from MinWon24.predictCaptcha import PredictCaptcha as MinWon24PredictCaptcha
 
 
-app = Flask('predict Captcha')
+app = Flask('innoCaptcha')
 api = Api(app)
 class CaptchaPredict(Resource) :
     
         def __init__(self, gpu_on = True) : 
-            #
-
-            self.predictor = PredictCaptcha()
-            self.r = request
             if not gpu_on :
                 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+                     
+            else :
+                gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+                for device in gpu_devices:
+                    tf.config.experimental.set_memory_growth(device, True)
+            
+            self.r = request
+            self.predictors = {}
+            self.predictors['BeobWon'] = BeobWonPredictCaptcha()
+            self.predictors['MinWon24'] = MinWon24PredictCaptcha()
         def post(self) :
             try :
                 # convert string of image data to uint8
                 img_array = np.frombuffer(base64.b64decode(self.r.data), np.uint8)     
                 # decode image
                 img_array = cv2.imdecode(img_array, cv2.IMREAD_UNCHANGED)
+                print(img_array.shape)
                 # predict
-                answer = self.predictor.predict(img_array)      
+                site = self.r.args.get('site')
+                print(site)
+                answer = self.predictors[site].predict(img_array)      
                 return {'answer' : answer}
             except Exception as e :
+                print(str(e))
                 return {'error' : str(e)}
             
 api.add_resource(CaptchaPredict, '/predict') 
 
 
 if __name__=='__main__' :
-    app.run(debug = False, port = 5000)
-
+    app.run(host='0.0.0.0',debug = False, port = 5000)
